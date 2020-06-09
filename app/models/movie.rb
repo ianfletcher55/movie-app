7fd8dc37520105849de1ea3ece11d273999f2c38
ianfletcher55 <ian.fletcher55@gmail.com>
2020-06-09 00:18:32 -0500

    adds active record queries

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,8 +1,8 @@
 class Movie < ApplicationRecord
 
-  validates :title, :director, length: { greater_than: 2 }
+  #validates :title, :director, length: { greater_than: 2 }
   validates :year, numericality: { greater_than_or_equal_to: 1900 }
   validates :plot, length: { in: 10...250 }
-  validates :english, presnece: true
+  #validates :english, presence: true
 
 end
